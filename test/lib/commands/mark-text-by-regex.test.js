31c54915f68d33b3b759234ad26e1de68f097eb4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-21 21:50:47 +1000

    Implemented MarkTextByRegex command

---
+++ test/lib/commands/mark-text-by-regex.test.js
@@ -0,0 +1,44 @@
+
+const MarkTextByRegexCommand = require('../../../lib/commands/mark-text-by-regex');
+
+suite('MarkTextByRegexCommand', () => {
+
+    test('it decorates text that matches to the specified regex', () => {
+        const vsWindow = {visibleTextEditors: ['EDITOR_1', 'EDITOR_2']};
+        const logger = getLogger();
+        const decorationOperator = {addDecoration: sinon.spy()};
+        const decorationOperatorFactory = {create: sinon.stub().returns(decorationOperator)};
+        const regexReader = {read: () => Promise.resolve(/pattern/)};
+        const command = new MarkTextByRegexCommand({decorationOperatorFactory, regexReader, vsWindow, logger});
+
+        return command.execute().then(() => {
+            expect(decorationOperatorFactory.create).to.have.been.calledWith(['EDITOR_1', 'EDITOR_2']);
+            expect(decorationOperator.addDecoration).to.have.been.calledWith(/pattern/);
+        });
+    });
+
+    test('it does nothing if regex is not given', () => {
+        const decorationOperatorFactory = {create: sinon.spy()};
+        const regexReader = {read: () => Promise.resolve()};
+        const command = new MarkTextByRegexCommand({decorationOperatorFactory, regexReader});
+
+        return command.execute().then(() => {
+            expect(decorationOperatorFactory.create).to.have.been.not.called;
+        });
+    });
+
+    test('it logs error if an exception occurred', () => {
+        const logger = {error: sinon.spy()};
+        const regexReader = {read: () => Promise.reject(new Error('UNEXPECTED_ERROR'))};
+        const command = new MarkTextByRegexCommand({logger, regexReader});
+
+        return command.execute().then(() => {
+            expect(logger.error.args[0][0]).to.have.string('Error: UNEXPECTED_ERROR');
+        });
+    });
+
+    function getLogger() {
+        return console;
+    }
+
+});
