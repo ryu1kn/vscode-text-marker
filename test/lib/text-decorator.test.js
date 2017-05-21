8c844670b644f6c862e73ad7bc3601124997da4b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-21 14:58:17 +1000

    TextDecorator now supports regex in addition to string

--- test/lib/text-decorator.test.js
+++ test/lib/text-decorator.test.js
@@ -3,16 +3,22 @@ const TextDecorator = require('../../lib/text-decorator');
 
 suite('TextDecorator', () => {
 
-    test('it decorates the text in the editors', () => {
+    test('it decorates the pattern in the editors', () => {
         const editors = [{setDecorations: sinon.spy()}, {setDecorations: sinon.spy()}];
         const textLocator = {
             locate: stubWithArgs(
-                [editors[0], 'TEXT'], ['RANGE1-1', 'RANGE1-2'],
-                [editors[1], 'TEXT'], ['RANGE2']
+                [editors[0], 'PATTERN'], ['RANGE1-1', 'RANGE1-2'],
+                [editors[1], 'PATTERN'], ['RANGE2']
             )
         };
         const textDecorator = new TextDecorator({textLocator});
-        textDecorator.decorate(editors, {TEXT: 'DECORATION_TYPE'});
+        textDecorator.decorate(
+            editors,
+            [{
+                pattern: 'PATTERN',
+                decorationType: 'DECORATION_TYPE'
+            }]
+        );
 
         expect(editors[0].setDecorations)
             .to.have.been.calledWith('DECORATION_TYPE', ['RANGE1-1', 'RANGE1-2']);
@@ -20,7 +26,7 @@ suite('TextDecorator', () => {
             .to.have.been.calledWith('DECORATION_TYPE', ['RANGE2']);
     });
 
-    test('it removes decorations from the text in the editors', () => {
+    test('it removes decorations from the pattern in the editors', () => {
         const editors = [{setDecorations: sinon.spy()}, {setDecorations: sinon.spy()}];
         const textDecorator = new TextDecorator({});
         textDecorator.undecorate(editors, ['DECORATION_TYPE_1', 'DECORATION_TYPE_2']);
@@ -39,12 +45,21 @@ suite('TextDecorator', () => {
         const editors = [{setDecorations: sinon.spy()}];
         const textLocator = {
             locate: stubWithArgs(
-                [editors[0], 'TEXT_1'], ['RANGE1-1', 'RANGE1-2'],
-                [editors[0], 'TEXT_2'], ['RANGE2']
+                [editors[0], 'PATTERN_1'], ['RANGE1-1', 'RANGE1-2'],
+                [editors[0], 'PATTERN_2'], ['RANGE2']
             )
         };
         const textDecorator = new TextDecorator({textLocator});
-        textDecorator.decorate(editors, {TEXT_1: 'DECORATION_TYPE', TEXT_2: null});
+        textDecorator.decorate(
+            editors,
+            [{
+                pattern: 'PATTERN_1',
+                decorationType: 'DECORATION_TYPE'
+            }, {
+                pattern: 'PATTERN_2',
+                decorationType: null
+            }]
+        );
 
         expect(editors[0].setDecorations.args).to.eql([
             ['DECORATION_TYPE', ['RANGE1-1', 'RANGE1-2']]
