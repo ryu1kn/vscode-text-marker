51524639bd615dc56d98998e48ca2817a5be391e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-21 16:26:57 +1000

    TextLocator now supports regex in addition to string

--- test/lib/text-locator.test.js
+++ test/lib/text-locator.test.js
@@ -3,7 +3,7 @@ const TextLocator = require('../../lib/text-locator');
 
 suite('TextLocator', () => {
 
-    test('it returns list of Range s which locates all the strings equals to the given text', () => {
+    test('it returns list of Range s which locates all the strings equal to the given text', () => {
         const Range = function (position1, position2) {
             return {start: position1, end: position2};
         };
@@ -12,6 +12,29 @@ suite('TextLocator', () => {
         expect(ranges).to.eql([
             {start: 'POSITION:7', end: 'POSITION:11'},
             {start: 'POSITION:12', end: 'POSITION:16'}
+        ]);
+    });
+
+    test('it returns list of Range s which locates all the strings match the given regex', () => {
+        const Range = function (position1, position2) {
+            return {start: position1, end: position2};
+        };
+        const textLocator = new TextLocator({Range});
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z ZZ'), /Z+/);
+        expect(ranges).to.eql([
+            {start: 'POSITION:12', end: 'POSITION:13'},
+            {start: 'POSITION:14', end: 'POSITION:16'}
+        ]);
+    });
+
+    test('it does not convert text expression into regex', () => {
+        const Range = function (position1, position2) {
+            return {start: position1, end: position2};
+        };
+        const textLocator = new TextLocator({Range});
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z+ ZZ'), 'Z+');
+        expect(ranges).to.eql([
+            {start: 'POSITION:12', end: 'POSITION:14'}
         ]);
     });
 
