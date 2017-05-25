d1706b18ce760c041adf968d6d6c3e9df827cf18
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-25 22:28:23 +1000

    WIP: Do not use RegExp/String directly to represent pattern

--- test/lib/text-locator.test.js
+++ test/lib/text-locator.test.js
@@ -4,11 +4,13 @@ const TextLocator = require('../../lib/text-locator');
 suite('TextLocator', () => {
 
     test('it returns list of Range s which locates all the strings equal to the given text', () => {
-        const Range = function (position1, position2) {
-            return {start: position1, end: position2};
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'String',
+            value: 'LONG',
+            ignoreCase: false
         };
-        const textLocator = new TextLocator({Range});
-        const ranges = textLocator.locate(fakeEditor('ENTIRE LONG LONG TEXT'), 'LONG');
+        const ranges = textLocator.locate(fakeEditor('ENTIRE LONG LONG TEXT'), pattern);
         expect(ranges).to.eql([
             {start: 'POSITION:7', end: 'POSITION:11'},
             {start: 'POSITION:12', end: 'POSITION:16'}
@@ -16,46 +18,81 @@ suite('TextLocator', () => {
     });
 
     test('it returns list of Range s which locates all the strings match the given regex', () => {
-        const Range = function (position1, position2) {
-            return {start: position1, end: position2};
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'RegExp',
+            value: 'Z+',
+            ignoreCase: false
         };
-        const textLocator = new TextLocator({Range});
-        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z ZZ'), /Z+/);
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z ZZ'), pattern);
         expect(ranges).to.eql([
             {start: 'POSITION:12', end: 'POSITION:13'},
             {start: 'POSITION:14', end: 'POSITION:16'}
         ]);
     });
 
-    test('it also works when the given regex has `g` flag set', () => {
-        const Range = function (position1, position2) {
-            return {start: position1, end: position2};
+    test('it returns list of Range s which locates all the strings match the given regex if ignore case', () => {
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'RegExp',
+            value: 'Z+',
+            ignoreCase: true
         };
-        const textLocator = new TextLocator({Range});
-        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z ZZ'), /Z+/gi);
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT z ZZ'), pattern);
         expect(ranges).to.eql([
             {start: 'POSITION:12', end: 'POSITION:13'},
             {start: 'POSITION:14', end: 'POSITION:16'}
+        ]);
+    });
+
+    test('it finds Range s which locates all the strings equal to the given text', () => {
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'String',
+            value: 'TEXT',
+            ignoreCase: false
+        };
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT text'), pattern);
+        expect(ranges).to.eql([
+            {start: 'POSITION:7', end: 'POSITION:11'}
+        ]);
+    });
+
+    test('it returns list of Range s which locates all the strings match the given text if ignore case', () => {
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'String',
+            value: 'text',
+            ignoreCase: true
+        };
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT text'), pattern);
+        expect(ranges).to.eql([
+            {start: 'POSITION:7', end: 'POSITION:11'},
+            {start: 'POSITION:12', end: 'POSITION:16'}
         ]);
     });
 
     test('it does not convert text expression into regex', () => {
-        const Range = function (position1, position2) {
-            return {start: position1, end: position2};
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'String',
+            value: 'Z+',
+            ignoreCase: false
         };
-        const textLocator = new TextLocator({Range});
-        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z+ ZZ'), 'Z+');
+        const ranges = textLocator.locate(fakeEditor('ENTIRE TEXT Z+ ZZ'), pattern);
         expect(ranges).to.eql([
             {start: 'POSITION:12', end: 'POSITION:14'}
         ]);
     });
 
     test('it does not get stuck with matching an empty string match', () => {
-        const Range = function (position1, position2) {
-            return {start: position1, end: position2};
+        const textLocator = new TextLocator({Range: fakeRange});
+        const pattern = {
+            type: 'RegExp',
+            value: '.*',
+            ignoreCase: false
         };
-        const textLocator = new TextLocator({Range});
-        const ranges = textLocator.locate(fakeEditor('ENTIRE\n\nTEXT'), /.*/);
+        const ranges = textLocator.locate(fakeEditor('ENTIRE\n\nTEXT'), pattern);
         expect(ranges).to.eql([
             {start: 'POSITION:0', end: 'POSITION:6'},
             {start: 'POSITION:8', end: 'POSITION:12'}
@@ -69,6 +106,10 @@ suite('TextLocator', () => {
                 positionAt: offset => `POSITION:${offset}`
             }
         };
+    }
+
+    function fakeRange(position1, position2) {
+        return {start: position1, end: position2};
     }
 
 });
