bf21864136b227fce0b47d83b1c84de510ae7c10
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-28 01:13:03 +1000

    Instantiate Pattern instead of raw RegExp in RegexReader

--- test/lib/regex-reader.test.js
+++ test/lib/regex-reader.test.js
@@ -4,12 +4,20 @@ const RegexReader = require('../../lib/regex-reader');
 suite('RegexReader', () => {
 
     test('shows inputBox to let user enter regex', () => {
+        const patternFactory = {create: sinon.stub().returns('PATTERN')};
         const vscodeWindow = {
-            showInputBox: sinon.stub().returns(Promise.resolve('pattern'))
+            showInputBox: sinon.stub().returns(Promise.resolve('PATTERN_STRING'))
         };
-        const reader = new RegexReader({vsWindow: vscodeWindow});
-        return reader.read().then(regex => {
-            expect(regex).to.eql(/pattern/);
+        const reader = new RegexReader({
+            patternFactory,
+            vsWindow: vscodeWindow
+        });
+        return reader.read().then(pattern => {
+            expect(pattern).to.eql('PATTERN');
+            expect(patternFactory.create).to.have.been.calledWith({
+                type: 'RegExp',
+                pattern: 'PATTERN_STRING'
+            });
             expect(vscodeWindow.showInputBox).to.have.been.calledWith({
                 placeHolder: 'Enter a regular expression to highlight text'
             });
