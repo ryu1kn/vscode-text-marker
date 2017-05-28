7a14656181b9efccbd2b8d48243f4b27c5afb8ad
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-28 21:44:47 +1000

    Show [Aa] symbol on picker if a pattern is case insensitive

--- test/lib/highlight-pattern-picker.test.js
+++ test/lib/highlight-pattern-picker.test.js
@@ -17,12 +17,12 @@ suite('HighlightPatternPicker', () => {
                 {
                     id: 'DECORATION_ID_1',
                     decorationType: 'DECORATION_TYPE_1',
-                    pattern: patternFactory.create({phrase: 'TEXT_1'})
+                    pattern: patternFactory.create({phrase: 'TEXT_1', ignoreCase: true})
                 },
                 {
                     id: 'DECORATION_ID_2',
                     decorationType: 'DECORATION_TYPE_2',
-                    pattern: patternFactory.create({phrase: 'TEXT_2', type: 'RegExp'})
+                    pattern: patternFactory.create({phrase: 'TEXT_2', type: 'RegExp', ignoreCase: true})
                 }
             ]
         };
@@ -35,6 +35,25 @@ suite('HighlightPatternPicker', () => {
                     {label: '/TEXT_2/', detail: 'RegExp', id: 'DECORATION_ID_2'}
                 ],
                 {placeHolder: 'Select a pattern to remove highlight'}
+            );
+        });
+    });
+
+    test('it shows "[Aa]" symbol if a pattern is case sensitive', () => {
+        const vscodeWindow = {showQuickPick: sinon.stub().returns(Promise.resolve())};
+        const decorationRegistry = {
+            retrieveAll: () => [
+                {
+                    id: 'DECORATION_ID_1',
+                    decorationType: 'DECORATION_TYPE_1',
+                    pattern: patternFactory.create({phrase: 'TEXT_1'})
+                }
+            ]
+        };
+        const picker = new HighlightPatternPicker({decorationRegistry, vsWindow: vscodeWindow});
+        return picker.pick().then(_decorationId => {
+            expect(vscodeWindow.showQuickPick).to.have.been.calledWith(
+                [{label: 'TEXT_1', detail: 'String [Aa]', id: 'DECORATION_ID_1'}]
             );
         });
     });
