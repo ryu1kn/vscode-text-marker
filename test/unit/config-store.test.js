f5dc18f6d4ccae1cc15db459e184937a6b3100e5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-17 12:51:18 +1100

    ref #5 Added tests for ConfigStore

--- test/unit/config-store.test.js
+++ test/unit/config-store.test.js
@@ -2,11 +2,26 @@
 const ConfigStore = require('../../lib/config-store');
 
 suite('ConfigStore', () => {
+    let extensionConfig;
+    let configStore;
+
+    beforeEach(() => {
+        extensionConfig = {
+            get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE'),
+            update: sinon.spy()
+        };
+        const workspace = {getConfiguration: stubWithArgs(['textmarker'], extensionConfig)};
+        const configTargetPicker = {pick: () => 'CONFIG_TARGET'};
+        configStore = new ConfigStore({workspace, configTargetPicker});
+    });
 
     test('it returns the current config from vscode.workspace', () => {
-        const extensionConfig = {get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE')};
-        const workspace = {getConfiguration: stubWithArgs(['textmarker'], extensionConfig)};
-        const configStore = new ConfigStore({workspace});
         expect(configStore.get('CONFIG_NAME')).to.eql('CONFIG_VALUE');
+    });
+
+    test('it sets a config value for the specified location', async () => {
+        await configStore.set('CONFIG_NAME', 'CONFIG_VALUE');
+
+        expect(extensionConfig.update).to.have.been.calledWith('CONFIG_NAME', 'CONFIG_VALUE', 'CONFIG_TARGET');
     });
 });
