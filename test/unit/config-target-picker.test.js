ae14551e8a9725e30bf05586c9ad81466f34ef22
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-17 13:05:40 +1100

    ref #5 Added tests for ConfigTargetPicker

---
+++ test/unit/config-target-picker.test.js
@@ -0,0 +1,32 @@
+
+const ConfigTargetPicker = require('../../lib/config-target-picker');
+
+suite('ConfigTargetPicker', () => {
+
+    test('it lets user to select which level they want to save a config', async () => {
+        const windowComponent = {showQuickPick: stubWithArgs(
+            [
+                [{
+                    label: 'Global',
+                    value: 1
+                }, {
+                    label: 'Workspace',
+                    value: 2
+                }],
+                {placeHolder: 'Select which scope of settings to save highlights to'}
+            ],
+            Promise.resolve({label: 'Global', value: 1})
+        )};
+        const picker = new ConfigTargetPicker({windowComponent});
+
+        expect(await picker.pick()).to.eql(1);
+    });
+
+    test('it returns null if user didn\'t select anything', async () => {
+        const windowComponent = {showQuickPick: () => {}};
+        const picker = new ConfigTargetPicker({windowComponent});
+
+        expect(await picker.pick()).to.eql(null);
+    });
+
+});
