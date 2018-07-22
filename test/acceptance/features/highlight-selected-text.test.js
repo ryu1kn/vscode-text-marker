4ef4450cb21c8655cc1c72752a5d9fc3ea3209cc
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-07-22 17:25:01 +1000

    ref #18 Added a test that reproduces the bug

--- test/acceptance/features/highlight-selected-text.test.js
+++ test/acceptance/features/highlight-selected-text.test.js
@@ -5,41 +5,52 @@ const FakeVscodeBuilder = require('../lib/fake-vscode-builder');
 
 suite('Highlight command', () => {
 
-    const editorBuilder = new FakeEditorBuilder();
-    const vscodeBuilder = new FakeVscodeBuilder();
+    let editor1;
+    let editor2;
+    let command;
+    let fakeVscode;
 
-    test('highlights selected text', async () => {
-        const editors = [
-            editorBuilder.build({
-                wholeText: 'A TEXT B TEXT C',
-                selectedText: 'TEXT'
-            }),
-            editorBuilder.build({
-                wholeText: 'a TEXT'
-            })
-        ];
+    beforeEach(() => {
+        const editorBuilder = new FakeEditorBuilder();
+        const vscodeBuilder = new FakeVscodeBuilder();
+        editor1 = editorBuilder.build({
+            wholeText: 'A TEXT B TEXT C',
+            selectedText: 'TEXT'
+        });
+        editor2 = editorBuilder.build({
+            wholeText: 'a TEXT'
+        });
         const fakeContext = {subscriptions: []};
-        const fakeVscode = vscodeBuilder.build({editors: editors});
+        fakeVscode = vscodeBuilder.build({editors: [editor1, editor2]});
 
         AppIntegrator.create(fakeVscode, console).integrate(fakeContext);
 
-        const command = fakeVscode._commands['textmarker.toggleHighlight'];
+        command = fakeVscode._commands['textmarker.toggleHighlight'];
+    });
 
-        await command(editors[0]);
+    test('highlights selected text', async () => {
+        await command(editor1);
 
-        expect(editors[0].setDecorations.args).to.eql([[
+        expect(editor1.setDecorations.args).to.eql([[
             'DECORATION_TYPE_1',
             [
                 new fakeVscode.Range('POSITION:2', 'POSITION:6'),
                 new fakeVscode.Range('POSITION:9', 'POSITION:13')
             ]
         ]]);
-        expect(editors[1].setDecorations).to.have.been.calledWith(
+        expect(editor2.setDecorations.args).to.eql([[
             'DECORATION_TYPE_1',
             [
                 new fakeVscode.Range('POSITION:2', 'POSITION:6')
             ]
-        );
+        ]]);
     });
 
+    test.skip('unhighlight selected text if the exact text is already selected', async () => {
+        await command(editor1);
+        await command(editor1);
+
+        expect(editor1.setDecorations.args[1]).to.eql([['DECORATION_TYPE_1', []]]);
+        expect(editor2.setDecorations.args[1]).to.eql([['DECORATION_TYPE_1', []]]);
+    });
 });
