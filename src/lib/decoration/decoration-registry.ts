1c6dc2f729a4bc82efc87855035626f90ce50a3b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-09-11 20:14:19 +1000

    Removed TextDecorationCollection

--- src/lib/decoration/decoration-registry.ts
+++ src/lib/decoration/decoration-registry.ts
@@ -1,16 +1,18 @@
 import * as vscode from 'vscode';
-import TextDecorationCollection from './text-decoration-collection';
+import {TextEditorDecorationType} from 'vscode';
 import ConfigStore from '../config-store';
 import ColourRegistry from '../colour-registry';
 import Pattern from '../pattern/pattern';
 import {Decoration} from '../entities/decoration';
 import {Option} from 'fp-ts/lib/Option';
 import DecorationTypeCreator from './decoration-type-creator';
+import {OptionMap} from '../common/collection';
 
 export default class DecorationRegistry {
     private readonly colourRegistry: ColourRegistry;
     private readonly decorationTypeCreator: DecorationTypeCreator;
-    private readonly textDecorationMap: TextDecorationCollection;
+    private readonly map: OptionMap<Decoration>;
+    private readonly generateUuid: () => string;
 
     constructor(configStore: ConfigStore,
                 window: typeof vscode.window,
@@ -18,16 +20,17 @@ export default class DecorationRegistry {
         this.colourRegistry = new ColourRegistry(configStore);
         this.decorationTypeCreator = new DecorationTypeCreator(configStore, window);
 
-        this.textDecorationMap = new TextDecorationCollection(generateUuid);
+        this.generateUuid = generateUuid;
+        this.map = new OptionMap();
     }
 
     inquireById(decorationId: string): Option<Decoration> {
-        return this.textDecorationMap.get(decorationId);
+        return this.map.get(decorationId);
     }
 
     inquireByPattern(pattern: Pattern): Option<Decoration> {
         const isSamePattern = (decoration: Decoration) => decoration.pattern.equalTo(pattern);
-        return this.textDecorationMap.find(isSamePattern);
+        return this.map.find(isSamePattern);
     }
 
     issue(pattern: Pattern): Decoration | null {
@@ -36,11 +39,18 @@ export default class DecorationRegistry {
 
         const colour = this.colourRegistry.issue();
         const decorationType = this.decorationTypeCreator.create(colour);
-        return this.textDecorationMap.add(pattern, colour, decorationType);
+        return this.add(pattern, colour, decorationType);
+    }
+
+    private add(pattern: Pattern, colour: string, decorationType: TextEditorDecorationType): Decoration {
+        const id = this.generateUuid();
+        const decoration = {id, pattern, colour, decorationType};
+        this.map.set(id, decoration);
+        return decoration;
     }
 
     updatePattern(decorationId: string, newPattern: Pattern): Option<Decoration> {
-        const decoration = this.textDecorationMap.get(decorationId);
+        const decoration = this.map.get(decorationId);
         return decoration.map(d => {
             d.pattern = newPattern;
             return d;
@@ -48,14 +58,14 @@ export default class DecorationRegistry {
     }
 
     revoke(decorationId: string): void {
-        const decoration = this.textDecorationMap.get(decorationId);
+        const decoration = this.map.get(decorationId);
         decoration.map(d => {
             this.colourRegistry.revoke(d.colour);
-            this.textDecorationMap.remove(decorationId);
+            this.map.delete(decorationId);
         });
     }
 
     retrieveAll() {
-        return this.textDecorationMap.toList();
+        return this.map.toList();
     }
 }
