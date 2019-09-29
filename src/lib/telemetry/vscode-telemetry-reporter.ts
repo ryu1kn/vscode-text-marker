b13a9a6411da4699187d1b97da3f2a28b6b75351
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-29 22:09:28 +1000

    Removed unnecessary export

--- src/lib/telemetry/vscode-telemetry-reporter.ts
+++ src/lib/telemetry/vscode-telemetry-reporter.ts
@@ -7,7 +7,7 @@ export interface VsTelemetryReporterLike {
 
 export type VsTelemetryReporterCreator = (extensionId: string, extensionVersion: string, telemetryKey: string) => VsTelemetryReporterLike;
 
-export const getVsTelemetryReporterCreator = (useRealReporter: boolean) => {
+export const getVsTelemetryReporterCreator = (useRealReporter: boolean): VsTelemetryReporterCreator => {
     if (useRealReporter) {
         // Cannot import at the top-level as it causes error during unit test
         const VsTelemetryReporter = require('vscode-extension-telemetry').default;
@@ -17,7 +17,7 @@ export const getVsTelemetryReporterCreator = (useRealReporter: boolean) => {
     }
 };
 
-export class NullVsTelemetryReporter implements VsTelemetryReporterLike {
+class NullVsTelemetryReporter implements VsTelemetryReporterLike {
     sendTelemetryEvent(_eventName: string, _properties?: ObjectMap<string>, _measurements?: ObjectMap<number>): void {
     }
 
