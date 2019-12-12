5fb0b3da4d028149e8458ccf04179c0bcfb94298
ctf0 <muah003@gmail.com>
2019-12-12 15:40:53 +0200

    mark on selection
    - fix https://github.com/ryu1kn/vscode-text-marker/issues/41

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import {getVsTelemetryReporterCreator} from './lib/telemetry/vscode-telemetry-re
 import {join} from 'path';
 import WorkspaceAdaptor from './lib/vscode/workspace';
 import {TelemetryReporterLocator} from './lib/telemetry/telemetry-reporter-locator';
+const debounce = require('lodash.debounce')
 
 const workspace = new WorkspaceAdaptor(vscode.workspace);
 const reporterCreator = getVsTelemetryReporterCreator(workspace.get<boolean>('enableTelemetry'));
@@ -15,6 +16,19 @@ const telemetryReporter = TelemetryReporterLocator.getReporter();
 exports.activate = (context: vscode.ExtensionContext) => {
     AppIntegrator.create(vscode as any, console).integrate(context);
     context.subscriptions.push(telemetryReporter);
+
+    // highlight selection
+    if (vscode.workspace.getConfiguration('textmarker.enableAutoHighlight')) {
+        vscode.window.onDidChangeTextEditorSelection(
+            debounce(function (e: vscode.TextEditorSelectionChangeEvent) {
+                vscode.commands.executeCommand('textmarker.clearAllHighlight')
+                
+                setTimeout(() => {
+                    vscode.commands.executeCommand('textmarker.toggleHighlight')
+                }, 100);
+            }, 150)
+        )
+    }
 };
 
 exports.deactivate = () => {
